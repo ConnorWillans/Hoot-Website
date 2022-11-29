333fd21d9761b6cdaaa12d1df08d85d6e77c68fb
ConnorWillans <connorgoal@gmail.com>
2022-11-29 10:58:49 -0500

    Upload

---
+++ Hoot/Hoot/routes/auth.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const authController = require('../controllers/auth');
+
+const router = express.Router();
+router.use(cookieParser());
+
+router.post('/register', authController.register);
+router.post('/login', authController.login);
+router.post('/post', authController.post);
+router.get('/logout', authController.logout);
+router.get('/get', authController.authenticate);
+
+module.exports = router;
